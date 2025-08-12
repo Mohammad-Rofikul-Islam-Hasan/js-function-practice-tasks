29c055b659b343edab07f4a9bdf7dc8d3d30496b
Mohammad-Rofikul-Islam-Hasan <rofikulislam.ph@gmail.com>
2025-08-13 03:47:03 +0600

    Task-2 Solution Added

--- js-function-practice-tasks-solutions.js
+++ js-function-practice-tasks-solutions.js
@@ -5,3 +5,12 @@ function multiplication(num1, num2, num3, num4) {
 }
 
 console.log(multiplication(2, 4, 6, 8));
+
+// Task 2 Solution
+
+function oddEven(num) {
+  return num % 2 === 1 ? num * 2 : num / 2;
+}
+
+console.log(oddEven(5));
+console.log(oddEven(6));
